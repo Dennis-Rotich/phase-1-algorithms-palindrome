850b0f76c98db1b212f1fbef01e7142df7fbda76
Dennis <denkipyator@gmail.com>
2024-01-13 10:53:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
+  let result = word.split('');
+  result = result.reverse();
+  result = result.join('');
+  if(result === word){
+    return true;
+  }
+  else{ return false};
   // Write your algorithm here
 }
 
 /* 
-  Add your pseudocode here
+   Add your pseudocode here
+   turn the string into an array using split method and saving to variable
+   use the reverse method on the array
+   join the array
+   compare the original word with reversed word
+   if they are similar print out true, if not print out false
 */
 
 /*
   Add written explanation of your solution here
+  We first create a new array by splitting the inputted word into individual characters.
+  We then reverse the character placement of the inputted word in the array.
+  Finally we join all the elements within the array back together into one single string.
+  If this newly created string matches the original word, it returns true meaning that the word is a palindrome
 */
 
 // You can run `node index.js` to view these console logs
